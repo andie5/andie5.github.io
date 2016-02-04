413b221a00cb6e9fdb74283f4baa8d720e27b7c1
Andie <andie5@users.noreply.github.com>
2016-02-04 23:45:16 +0000

    Create a custom alert message Customised the buttons from bootstrap in the multiply function Increased sizing of text Updated the help function for multiplication with hidden array Star image added

--- calculate.js
+++ calculate.js
@@ -23,9 +23,16 @@ function generateNumbers(){
 	prepareQuestions();
 
 	//Set the hint images to blank
-	document.getElementById("helpImage1").innerHTML = "";
-	document.getElementById("helpImage2").innerHTML = "";
+//	starBox.style.display = 'none';
+//	if(document.getElementById("helpImage1")!= null && document.getElementById("helpImage2") != null){
+	// if(document.getElementById("helpImage1")) {
+	// 	document.getElementById("helpImage1").innerHTML = "";
+	// }
+	// if(document.getElementById("helpImage2")) {
+	// 	document.getElementById("helpImage2").innerHTML = "";
+	// }
 
+//	}
 }
 
 //Prepare the array of questions
@@ -56,11 +63,11 @@ function prepareQuestions(){
 
 	multiplyQuestions = ["Ben has "+number1+" counter(s) in every tin. There are "+number2+" tin(s). How many are counters altogether?",
 	"Liz has "+number1+" coloured bead(s) in each of her "+number2+" baskets, how many beads altogether?",
-	"Mary has "+number1+" earring(s) in each box. There are "+number2+" boxe(s). What is the total number of earrings?",
+	"Mary has "+number1+" earring(s) in each box. There are "+number2+" boxes. What is the total number of earrings?",
 	"Albert has "+number1+" coloured sticker(s) on every page of his book. There are "+number2+" page(s). How many coloured stickers altogether?",
-	"Timothy has "+number2+" mixed fruit in every bowl. There are "+number2+" bowl(s). How much mixed fruit altogether",
+	"Timothy has "+number2+" mixed fruit in every bowl. There are "+number2+" bowl(s). How much mixed fruit altogether?",
 	"Tino has "+number1+" bean(s) in every packet. There are "+number2+" packet(s). How many beans altogether?",
-	"Thuli has "+number1+" jelly bean(s) in each bag. There are "+number2+" bags(s). what is the total number of jelly beans?",
+	"Thuli has "+number1+" jelly bean(s) in each bag. There are "+number2+" bags(s). What is the total number of jelly beans?",
 	"Tandana has "+number1+" ball(s) in each basket. There are "+number2+" basket(s). How many balls altogether?",
 	"Marie has "+number1+" magnet(s) in each drawer. There are "+number2+" drawer(s). How many drawers altogether?",
 	"Fiona has "+number1+" coloured rock(s) in each box. There are "+number2+" boxes. How many coloured rocks altogether?"
@@ -110,7 +117,7 @@ function setPageNumbers(operator){
 2- Create a handle on the submit answer button
 3- Do the calculation, then call the function to check the user's answer*/
 function add(){
-	+setPageNumbers("+");
+	setPageNumbers("+");
 
 	var answer = document.getElementById("submitAns");
 	var correctAns = number1 + number2;
@@ -119,7 +126,7 @@ function add(){
 		checkUserAnswer("add", userAns, correctAns);
 	}
 	//Hint for the question
-	questionHelp();
+	questionHelp("add");
 }
 
 /**Calculate the subtraction of the 2 random numbers that are generated
@@ -142,7 +149,7 @@ function minus(){
 
 		checkUserAnswer("minus", userAns, correctAns);
 	}
-	questionHelp();
+	questionHelp("minus");
 };
 
 
@@ -161,7 +168,7 @@ function multiply(){
 
 		checkUserAnswer("multiply", userAns, correctAns);
 	}
-
+	questionHelp("multiply");
 };
 
 
@@ -185,12 +192,12 @@ function divide(){
 		var userAns = document.getElementById("answer").value;
 		//var correctAns = number1 / number2;
 
-		//Round the division for simplicity for 5-7 year olds, you want to return the largest equal share of the su
+		//Round the division for simplicity for 5-7 year olds, you want to return the largest equal share of the sum
 		// Doesn't deal with the remainder
 		var correctAns = Math.floor(number1 / number2);
-
 		checkUserAnswer("divide", userAns, correctAns);
 	}
+	questionHelp("divide");
 };
 
 /*Validation checks on the user's answer
@@ -200,18 +207,18 @@ generated*/
 function checkUserAnswer(type, userAns, correctAns){
 	// if (count < maxTries) {
 	if(userAns === ""){
-		alert("Please enter an answer");
+		messageBox("Please enter an answer.");
 	}
 	else if(isNaN(parseInt(userAns))){
-		alert("Please enter a number");
+		messageBox("Please enter a number.");
 	}
 	else if (parseInt(userAns) != correctAns)
 	{
 		//count++;
-		alert("That is the incorrect answer");
+		messageBox("Sorry that is the incorrect answer. Please try again.");
 	}
 	else if (parseInt(userAns) === correctAns)		{
-		alert("That is the correct answer");
+		messageBox("Well done! That is the correct answer.");
 		document.getElementById("answer").value = "";
 		generateNumbers();
 
@@ -234,28 +241,91 @@ function checkUserAnswer(type, userAns, correctAns){
 	// }
 }
 
+/*Message to the user*/
+function messageBox(message){
+	//console.log("message box called");
+	document.getElementById("msgBox").innerHTML = message;
+ 	var bckgrdMsg = document.getElementById('backgroundMsgBox');
+	var closeMsgBox = document.getElementById("closeMsg");
+	bckgrdMsg.style.display = 'inline';
+	closeMsgBox.style.display = 'block';
+// Display thew background as none by default
+//use toggle classes to show the background and msgBox
+// use button to toggle classes background
+// z-index for foreground and background - higher z index more to the front it will appearing
+// 3 ways to make something disappear display:none, opacity: 0 or z-index -99
+
+	closeMsgBox.onclick =  function() {
+		bckgrdMsg.style.display = 'none';
+	}
+}
+
 /*This will help to provide a visual representation of the question*/
-function questionHelp()
+function questionHelp(sign)
 {
 	var hint = document.getElementById("help");
 
 	hint.onclick =  function() {
 		var counter="";
 		var counter2="";
-		var imageSrc = '<img src="http://images.clipartpanda.com/apple-clipart-apple5.png" alt="Apple image" width="20" height="40" />'
-		for(var i=0; i<number1; i++){
-			 counter = imageSrc + counter;
+		var imageSrc = '<img src="star.png" alt="Apple image" width="30" height="40" />'
+		var imageArray = [];
+
+		if(sign ==="add" || sign === "minus") {
+			for(var i=0; i<number1; i++){
+				 counter = imageSrc + counter;
+			}
+			for(var i=0; i<number2; i++){
+				 counter2 = imageSrc + counter2;
+			}
+			document.getElementById("helpImage1").innerHTML = counter;
+			document.getElementById("helpImage2").innerHTML = counter2;
 		}
-		for(var i=0; i<number2; i++){
-			 counter2 = imageSrc + counter2;
+		else if(sign ==="multiply"){
+			// for(var i=0; i<number2; i++){
+			// 	imageArray[i] = "<div>"
+			// 	for(var j=0; j<number1; j++){
+			// 		imageArray[i] = imageArray[i] + imageSrc;
+			// 	}
+			// 	imageArray[i] = imageArray[i] + "</div>";
+			// }
+			// console.log(imageArray);
+			// document.getElementById("helpImage1").innerHTML = imageArray;
+			var starWidth = number1 * 45;
+			var starHeight = number2 * 50;
+
+			var starBox = document.getElementById("stars")
+			starBox.style.width = starWidth + "px";
+			starBox.style.height = starHeight + "px";
+			starBox.style.display = 'block';
 		}
-		document.getElementById("helpImage1").innerHTML = counter;
-		document.getElementById("helpImage2").innerHTML = counter2;
+		else {
+			//For division
+			var correctAns = Math.floor(number1 / number2);
+
+			//This might be helpful later
+		//	var remainder = number1 % number2;
+
+			// for(var i=0; i<number2; i++){
+			// 	var starElem;
+			// 	for(var j=0; j<correctAns; j++){
+			// 		imageArray[i] = imageArray[i] + imageSrc;
+			// 	}
+			// 	imageArray[i] = imageArray[i] + "</div>";
+			// }
+			// console.log(remainder);
+			// document.getElementById("helpImage1").innerHTML = imageArray;
+
+			var starWidth = correctAns * 45;
+			var starHeight = number2 * 50;
+
+			var starBox = document.getElementById("stars")
+			starBox.style.width = starWidth + "px";
+			starBox.style.height = starHeight + "px";
+			starBox.style.display = 'block';
+		}
 	}
 }
 
 // Call the generate the random numbers as soon as the page is open
 generateNumbers();
-
-// window.onload = function(){
-// };
