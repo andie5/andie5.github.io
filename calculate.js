5075316bbb7f0ede7723d5bf45562d20880c61c7
Andie <andie5@users.noreply.github.com>
2016-02-14 17:39:26 +0000

    Refactored the questions into another file Stored the random numbers in a local variable, so they are passed by functions rather than stored globally Added a map (key-value) to deal with the key(arithmetic function) and the value(function to return a specific question from the array of questions)

--- calculate.js
+++ calculate.js
@@ -1,132 +1,53 @@
-//Mathematical functions
-// Stores the 2 random numbers generated
-var number1;
-var number2;
+/*Mathematical functions*/
+
 //The maximum number used in any sum - smaller number to keep it easy for 5-7 year olds
 var maxNum = 10;
-var randomQuNum;
-// var maxTries = 5;
-// var count = 0;
-var addQuestions = [];
-var minusQuestions = [];
-var multiplyQuestions = [];
-var divideQuestions = [];
 
-// Generate the random numbers and store them
 function generateNumbers(){
-	number1 = Math.ceil(Math.random()*maxNum);
-	number2 = Math.ceil(Math.random()*maxNum);
+//Generate random numbers
+	var numbers = [];
+	for(var i = 0; i< 2; i++){
+		numbers[i] = Math.ceil(Math.random()*maxNum);
+	}
+	//Swap numbers around if second number is more than the first.
+	numbers.sort(function(a, b) {
+  	return b - a;
+	});
+	return numbers;
+}
+
+function getQuestionIndex(){
 	//Multiply by 10 as only 10 questions for each of the operators
-	randomQuNum = Math.floor(Math.random()*10);
-
-	// Set up the questions
-	prepareQuestions();
-
-	//Set the hint images to blank
-//	starBox.style.display = 'none';
-//	if(document.getElementById("helpImage1")!= null && document.getElementById("helpImage2") != null){
-	// if(document.getElementById("helpImage1")) {
-	// 	document.getElementById("helpImage1").innerHTML = "";
-	// }
-	// if(document.getElementById("helpImage2")) {
-	// 	document.getElementById("helpImage2").innerHTML = "";
-	// }
-
-//	}
+	return Math.floor(Math.random()*10);
 }
 
-//Prepare the array of questions
-function prepareQuestions(){
-	addQuestions = ["Ben has "+number1+" counter(s) and Tilly has "+number2+" counter(s), how many are counters altogether?",
-	"David and Liz have "+number1+" coloured bead(s) and "+number2+" coloured bead(s), how many beads altogether?",
-	"Mary has "+number1+" earring(s) and Tina has "+number2+" earring(s), what is the total number of earrings?",
-	"Robert and Albert have "+number1+" coloured sticker(s) and "+number2+" coloured sticker(s), how many coloured stickers altogether?",
-	"David has "+number1+" mixed fruit and Liz has "+number2+" mixed fruit, what is the total number of mixed fruit?",
-	"Tino and Rita have "+number1+" bean(s) and "+number2+" bean(s), how many beans altogether?",
-	"Thuli has "+number1+" jelly bean(s) and Ariko has "+number2+" jelly bean(s), what is the total number of jelly beans?",
-	"Tandana has "+number1+" ball(s) and Flora has ball(s), how many balls?",
-	"Marie has "+number1+" magnet(s) and Lewis has magnet(s), what is the total number of magnets?",
-	"Fiona has "+number1+" coloured rock(s) and Daniel has "+number2+" coloured rock(s), what is the total number of coloured rocks?"
-	];
-
-	minusQuestions = ["Ben has "+number1+" counter(s) and gives Tilly "+number2+" counter(s), how many are counters does Ben now have?",
-	"Emmanuel has "+number1+" coloured bead(s) and drops "+number2+" coloured bead(s), how many beads does David have left?",
-	"Mary has "+number1+" earring(s) and puts "+number2+" earring(s) in a box, how many earrings are left?",
-	"Robert had "+number1+" coloured sticker(s) but has lost "+number2+" coloured sticker(s), how many coloured stickers does Robert have left?",
-	"David has "+number1+" pieces of mixed fruit and eats "+number2+", how many does David have left?",
-	"Tino has "+number1+" bean(s) and has eaten "+number2+" beans, how many bean(s) does Tino have left to eat?",
-	"Thuli has "+number1+" jelly bean(s) has given Ariko "+number2+" jelly bean(s), how many jelly beans does Thuli have now?",
-	"Tandana has "+number1+" ball(s) and has left "+number2+" ball(s) in the shed, how many balls does Tandana have to play with?",
-	"Marie has "+number1+" magnet(s) and gives Lewis "+number2+" magnet(s), how many magnets does Marie have?",
-	"Fiona has collected "+number1+" coloured rock(s) but left "+number2+" coloured rock(s) behind, how many coloured rocks does Fiona have?"
-	];
-
-	multiplyQuestions = ["Ben has "+number1+" counter(s) in every tin. There are "+number2+" tin(s). How many are counters altogether?",
-	"Liz has "+number1+" coloured bead(s) in each of her "+number2+" baskets, how many beads altogether?",
-	"Mary has "+number1+" earring(s) in each box. There are "+number2+" boxes. What is the total number of earrings?",
-	"Albert has "+number1+" coloured sticker(s) on every page of his book. There are "+number2+" page(s). How many coloured stickers altogether?",
-	"Timothy has "+number2+" mixed fruit in every bowl. There are "+number2+" bowl(s). How much mixed fruit altogether?",
-	"Tino has "+number1+" bean(s) in every packet. There are "+number2+" packet(s). How many beans altogether?",
-	"Thuli has "+number1+" jelly bean(s) in each bag. There are "+number2+" bags(s). What is the total number of jelly beans?",
-	"Tandana has "+number1+" ball(s) in each basket. There are "+number2+" basket(s). How many balls altogether?",
-	"Marie has "+number1+" magnet(s) in each drawer. There are "+number2+" drawer(s). How many drawers altogether?",
-	"Fiona has "+number1+" coloured rock(s) in each box. There are "+number2+" boxes. How many coloured rocks altogether?"
-	];
-
-
-	divideQuestions = ["Ben has "+number1+" counter(s) and "+number2+" friends, if Ben shares the counter(s) equally how many can each of them have? There may be some left over.",
-	"David has "+number1+" coloured bead(s) and "+number2+" boxes, if David shares the bead(s) equally how many in each box? There may be some left over.",
-	"Mary has "+number1+" earring(s) and wants to share them equally with her "+number2+" sister(s), how many does each sister get? There may be some left over.",
-	"Pearl has "+number1+" coloured sticker(s) and wants to put the same number of stickers on "+number2+" books(s), how many stickers on each book? There may be some left over.",
-	"David has "+number1+" mixed fruit and "+number2+" friends, if David shares the mixed fruit equally how many can each of them have? There may be some left over that David can eat.",
-	"Rita havs "+number1+" bean(s) and "+number2+" boxes. If Rita shares the bean(s) equally how many in each box? There may be some left over.",
-	"Thuli has "+number1+" jelly bean(s) and "+number2+" brother(s), if Thuli shares the bean(s) equally how many can each of them have? There may be some left over.",
-	"Tandana has "+number1+" ball(s) and "+number2+" basket(s). If Tandana puts the same number of in each basket, how many balls in each basket? There may be some left over.",
-	"Marie has "+number1+" magnet(s) and "+number2+" fridge(s). If Marie puts the same number of magnet(s) on each fridge, how many magnets are on each fridge? There may be some left over.",
-	"Fiona has "+number1+" coloured rock(s) and "+number2+" bags. If Fiona puts the same number of coloured rock(s) in each bag, how many coloured rocks are in each bag? There may be some left over.",,
-	];
-}
-
-//Display the random numbers generated
-function setPageNumbers(operator){
-
-	//Display a question from the arrays by using one of the random numbers generated from earlier to select
-	// a random question from the relevant array of predefined questions.
-	var question= "";
-
-	if(operator == "+")	{
-		question = addQuestions[randomQuNum];
-	}
-	else if(operator == "-")	{
-		question = minusQuestions[randomQuNum];
-	}
-	else if(operator == "x") {
-		question = multiplyQuestions[randomQuNum];
-	}
-	else {
-		question = divideQuestions[randomQuNum];
-	}
-	document.getElementById("number1_sum").innerHTML = number1;
-	document.getElementById("number2_sum").innerHTML = number2;
+// Display a question from the arrays by using one of the random numbers generated from earlier to select
+// a random question from the relevant array of predefined questions.
+function updateDOM(numbers, operator, question){
+	document.getElementById("number1_sum").innerHTML = numbers[0];
+	document.getElementById("number2_sum").innerHTML = numbers[1];
 	document.getElementById("operator").innerHTML = operator;
 	document.getElementById("question").innerHTML = question;
-};
+}
 
 /**Calculate the sum of the 2 random numbers that are generated
 1- Display the calculation with the randomly generated numbers
 2- Create a handle on the submit answer button
 3- Do the calculation, then call the function to check the user's answer*/
 function add(){
-	setPageNumbers("+");
+	var numbers  = generateNumbers();
+	var index    = getQuestionIndex();
+	var question = window.questions['add'](numbers)[index];
+
+	updateDOM(numbers, '+', question);
 
 	var answer = document.getElementById("submitAns");
-	var correctAns = number1 + number2;
+	var correctAns = numbers[0] + numbers[1];
 	answer.onclick =  function() {
 		var userAns = document.getElementById("answer").value;
 		checkUserAnswer("add", userAns, correctAns);
 	}
-	//Hint for the question
-	questionHelp("add");
+	questionHelp("add", numbers);
 }
 
 /**Calculate the subtraction of the 2 random numbers that are generated
@@ -134,22 +55,20 @@ function add(){
 2- Create a handle on the submit answer button
 3- Do the calculation, then call the function to check the user's answer*/
 function minus(){
-	// To make the subtraction easier, we want the first number to be bigger than the second.
-	if (number1 < number2){
-		var tempNum = number1;
-		number1 = number2;
-		number2 = tempNum;
-	}
-	setPageNumbers("-");
+	//s[0] To make the subtraction easier, we want the first number to be bigger than the second.
+	var numbers  = generateNumbers();
+	var index    = getQuestionIndex();
+	var question = window.questions['minus'](numbers)[index];
+
+	updateDOM(numbers, '-', question);
 
 	var answer = document.getElementById("submitAns");
 	answer.onclick =  function() {
 		var userAns = document.getElementById("answer").value;
-		var correctAns = number1 - number2;
-
+		var correctAns = numbers[0] - numbers[1];
 		checkUserAnswer("minus", userAns, correctAns);
 	}
-	questionHelp("minus");
+	questionHelp("minus", numbers);
 };
 
 
@@ -158,17 +77,20 @@ function minus(){
 2- Create a handle on the submit answer button
 3- Do the calculation, then call the function to check the user's answer*/
 function multiply(){
-	setPageNumbers("x");
+	var numbers  = generateNumbers();
+	var index    = getQuestionIndex();
+	var question = window.questions['multiply'](numbers)[index];
+
+	updateDOM(numbers, 'x', question);
 
 	var answer = document.getElementById("submitAns");
 	answer.onclick =  function() {
-
 		var userAns = document.getElementById("answer").value;
-		var correctAns = number1 * number2;
+		var correctAns = numbers[0] * numbers[1];
 
 		checkUserAnswer("multiply", userAns, correctAns);
 	}
-	questionHelp("multiply");
+	questionHelp("multiply", numbers);
 };
 
 
@@ -177,27 +99,24 @@ function multiply(){
 2- Create a handle on the submit answer button
 3- Do the calculation, then call the function to check the user's answer*/
 function divide(){
-	// To make the division easier, we want the first number to be bigger than the second.
-	if (number1 < number2){
-		var tempNum = number1;
-		number1 = number2;
-		number2 = tempNum;
-	}
+
+	var numbers  = generateNumbers();
+	var index    = getQuestionIndex();
+	var question = window.questions['multiply'](numbers)[index];
+
 	//\xF7 hexadecimal for divide sign
-	setPageNumbers("\xF7");
+	updateDOM(numbers, '\xF7', question);
 
 	var answer = document.getElementById("submitAns");
 	answer.onclick =  function() {
-
 		var userAns = document.getElementById("answer").value;
-		//var correctAns = number1 / number2;
 
 		//Round the division for simplicity for 5-7 year olds, you want to return the largest equal share of the sum
 		// Doesn't deal with the remainder
-		var correctAns = Math.floor(number1 / number2);
+		var correctAns = Math.floor(numbers[0] / numbers[1]);
 		checkUserAnswer("divide", userAns, correctAns);
 	}
-	questionHelp("divide");
+	questionHelp("divide", numbers);
 };
 
 /*Validation checks on the user's answer
@@ -211,11 +130,13 @@ function checkUserAnswer(type, userAns, correctAns){
 	}
 	else if(isNaN(parseInt(userAns))){
 		messageBox("Please enter a number.");
+		document.getElementById("answer").value = "";
 	}
 	else if (parseInt(userAns) != correctAns)
 	{
 		//count++;
 		messageBox("Sorry that is the incorrect answer. Please try again.");
+		document.getElementById("answer").value = "";
 	}
 	else if (parseInt(userAns) === correctAns)		{
 		messageBox("Well done! That is the correct answer.");
@@ -249,11 +170,6 @@ function messageBox(message){
 	var closeMsgBox = document.getElementById("closeMsg");
 	bckgrdMsg.style.display = 'inline';
 	closeMsgBox.style.display = 'block';
-// Display thew background as none by default
-//use toggle classes to show the background and msgBox
-// use button to toggle classes background
-// z-index for foreground and background - higher z index more to the front it will appearing
-// 3 ways to make something disappear display:none, opacity: 0 or z-index -99
 
 	closeMsgBox.onclick =  function() {
 		bckgrdMsg.style.display = 'none';
@@ -261,7 +177,7 @@ function messageBox(message){
 }
 
 /*This will help to provide a visual representation of the question*/
-function questionHelp(sign)
+function questionHelp(sign, numbers)
 {
 	var hint = document.getElementById("help");
 
@@ -272,53 +188,28 @@ function questionHelp(sign)
 		var imageArray = [];
 
 		if(sign ==="add" || sign === "minus") {
-			for(var i=0; i<number1; i++){
+			for(var i=0; i<numbers[0]; i++){
 				 counter = imageSrc + counter;
 			}
-			for(var i=0; i<number2; i++){
+			for(var i=0; i<numbers[1]; i++){
 				 counter2 = imageSrc + counter2;
 			}
 			document.getElementById("helpImage1").innerHTML = counter;
 			document.getElementById("helpImage2").innerHTML = counter2;
 		}
 		else if(sign ==="multiply"){
-			// for(var i=0; i<number2; i++){
-			// 	imageArray[i] = "<div>"
-			// 	for(var j=0; j<number1; j++){
-			// 		imageArray[i] = imageArray[i] + imageSrc;
-			// 	}
-			// 	imageArray[i] = imageArray[i] + "</div>";
-			// }
-			// console.log(imageArray);
-			// document.getElementById("helpImage1").innerHTML = imageArray;
-			var starWidth = number1 * 45;
-			var starHeight = number2 * 50;
-
-			var starBox = document.getElementById("stars")
+			var starWidth = numbers[0] * 45;
+			var starHeight = numbers[1] * 50;
+			var starBox = document.getElementById("stars");
 			starBox.style.width = starWidth + "px";
 			starBox.style.height = starHeight + "px";
 			starBox.style.display = 'block';
 		}
 		else {
 			//For division
-			var correctAns = Math.floor(number1 / number2);
-
-			//This might be helpful later
-		//	var remainder = number1 % number2;
-
-			// for(var i=0; i<number2; i++){
-			// 	var starElem;
-			// 	for(var j=0; j<correctAns; j++){
-			// 		imageArray[i] = imageArray[i] + imageSrc;
-			// 	}
-			// 	imageArray[i] = imageArray[i] + "</div>";
-			// }
-			// console.log(remainder);
-			// document.getElementById("helpImage1").innerHTML = imageArray;
-
+			var correctAns = Math.floor(numbers[0] / numbers[1]);
 			var starWidth = correctAns * 45;
-			var starHeight = number2 * 50;
-
+			var starHeight = numbers[1] * 50;
 			var starBox = document.getElementById("stars")
 			starBox.style.width = starWidth + "px";
 			starBox.style.height = starHeight + "px";
---
+++ questions.js
@@ -0,0 +1,73 @@
+
+//Function to invoke the array of questions
+(function(whichFunc){
+//function(questions){
+  var addQuestions = function(numbers){
+    return [
+      "Ben has "+numbers[0]+" counter(s) and Tilly has "+numbers[1]+" counter(s), how many are counters altogether?",
+      "David and Liz have "+numbers[0]+" coloured bead(s) and "+numbers[1]+" coloured bead(s), how many beads altogether?",
+      "Mary has "+numbers[0]+" earring(s) and Tina has "+numbers[1]+" earring(s), what is the total number of earrings?",
+      "Robert and Albert have "+numbers[0]+" coloured sticker(s) and "+numbers[1]+" coloured sticker(s), how many coloured stickers altogether?",
+      "David has "+numbers[0]+" mixed fruit and Liz has "+numbers[1]+" mixed fruit, what is the total number of mixed fruit?",
+      "Tino and Rita have "+numbers[0]+" bean(s) and "+numbers[1]+" bean(s), how many beans altogether?",
+      "Thuli has "+numbers[0]+" jelly bean(s) and Ariko has "+numbers[1]+" jelly bean(s), what is the total number of jelly beans?",
+      "Tandana has "+numbers[0]+" ball(s) and Flora has ball(s), how many balls?",
+      "Marie has "+numbers[0]+" magnet(s) and Lewis has " +numbers[1]+ "magnet(s), what is the total number of magnets?",
+      "Fiona has "+numbers[0]+" coloured rock(s) and Daniel has "+numbers[1]+" coloured rock(s), what is the total number of coloured rocks?"
+    ];
+  }
+
+  var minusQuestions = function(numbers){
+    return [
+      "Ben has "+numbers[0]+" counter(s) and gives Tilly "+numbers[1]+" counter(s), how many are counters does Ben now have?",
+      "Emmanuel has "+numbers[0]+" coloured bead(s) and drops "+numbers[1]+" coloured bead(s), how many beads does David have left?",
+      "Mary has "+numbers[0]+" earring(s) and puts "+numbers[1]+" earring(s) in a box, how many earrings are left?",
+      "Robert had "+numbers[0]+" coloured sticker(s) but has lost "+numbers[1]+" coloured sticker(s), how many coloured stickers does Robert have left?",
+      "David has "+numbers[0]+" pieces of mixed fruit and eats "+numbers[1]+", how many does David have left?",
+      "Tino has "+numbers[0]+" bean(s) and has eaten "+numbers[1]+" beans, how many bean(s) does Tino have left to eat?",
+      "Thuli has "+numbers[0]+" jelly bean(s) and has given Ariko "+numbers[1]+" jelly bean(s), how many jelly beans does Thuli have now?",
+      "Tandana has "+numbers[0]+" ball(s) and has left "+numbers[1]+" ball(s) in the shed, how many balls does Tandana have to play with?",
+      "Marie has "+numbers[0]+" magnet(s) and gives Lewis "+numbers[1]+" magnet(s), how many magnets does Marie have?",
+      "Fiona has collected "+numbers[0]+" coloured rock(s) but left "+numbers[1]+" coloured rock(s) behind, how many coloured rocks does Fiona have?"
+    ];
+  }
+
+  var multiplyQuestions = function(numbers){
+    return [
+      "Ben has "+numbers[0]+" counter(s) in every tin. There are "+numbers[1]+" tin(s). How many are counters altogether?",
+      "Liz has "+numbers[0]+" coloured bead(s) in each of her "+numbers[1]+" baskets, how many beads altogether?",
+      "Mary has "+numbers[0]+" earring(s) in each box. There are "+numbers[1]+" boxes. What is the total number of earrings?",
+      "Albert has "+numbers[0]+" coloured sticker(s) on every page of his book. There are "+numbers[1]+" page(s). How many coloured stickers altogether?",
+      "Timothy has "+numbers[1]+" mixed fruit in every bowl. There are "+numbers[1]+" bowl(s). How much mixed fruit altogether?",
+      "Tino has "+numbers[0]+" bean(s) in every packet. There are "+numbers[1]+" packet(s). How many beans altogether?",
+      "Thuli has "+numbers[0]+" jelly bean(s) in each bag. There are "+numbers[1]+" bags(s). What is the total number of jelly beans?",
+      "Tandana has "+numbers[0]+" ball(s) in each basket. There are "+numbers[1]+" basket(s). How many balls altogether?",
+      "Marie has "+numbers[0]+" magnet(s) in each drawer. There are "+numbers[1]+" drawer(s). How many drawers altogether?",
+      "Fiona has "+numbers[0]+" coloured rock(s) in each box. There are "+numbers[1]+" boxes. How many coloured rocks altogether?"
+    ];
+  }
+
+  var divideQuestions = function(numbers){
+    return [
+      "Ben has "+numbers[0]+" counter(s) and "+numbers[1]+" friends, if Ben shares the counter(s) equally how many can each of them have? There may be some left over.",
+      "David has "+numbers[0]+" coloured bead(s) and "+numbers[1]+" boxes, if David shares the bead(s) equally how many in each box? There may be some left over.",
+      "Mary has "+numbers[0]+" earring(s) and wants to share them equally with her "+numbers[1]+" sister(s), how many does each sister get? There may be some left over.",
+      "Pearl has "+numbers[0]+" coloured sticker(s) and wants to put the same number of stickers on "+numbers[1]+" books(s), how many stickers on each book? There may be some left over.",
+      "David has "+numbers[0]+" mixed fruit and "+numbers[1]+" friends, if David shares the mixed fruit equally how many can each of them have? There may be some left over that David can eat.",
+      "Rita havs "+numbers[0]+" bean(s) and "+numbers[1]+" boxes. If Rita shares the bean(s) equally how many in each box? There may be some left over.",
+      "Thuli has "+numbers[0]+" jelly bean(s) and "+numbers[1]+" brother(s), if Thuli shares the bean(s) equally how many can each of them have? There may be some left over.",
+      "Tandana has "+numbers[0]+" ball(s) and "+numbers[1]+" basket(s). If Tandana puts the same number of in each basket, how many balls in each basket? There may be some left over.",
+      "Marie has "+numbers[0]+" magnet(s) and "+numbers[1]+" fridge(s). If Marie puts the same number of magnet(s) on each fridge, how many magnets are on each fridge? There may be some left over.",
+      "Fiona has "+numbers[0]+" coloured rock(s) and "+numbers[1]+" bags. If Fiona puts the same number of coloured rock(s) in each bag, how many coloured rocks are in each bag? There may be some left over.",,
+    ];
+  }
+
+  whichFunc.questions = {
+  //questions = {
+    add: addQuestions,
+    minus: minusQuestions,
+    multiply: multiplyQuestions,
+    divide: divideQuestions
+  }
+})(this);
+// console.log('window.questions['add']', window.questions['add']);
