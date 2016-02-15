1f12c3a52607568b22cafd1b6d8de4853004fc4d
Andie <andie5@users.noreply.github.com>
2016-02-15 23:45:09 +0000

    Changed the question phrase of boxes to box/boxes

--- questions.js
+++ questions.js
@@ -36,25 +36,25 @@
     return [
       "Ben has "+numbers[0]+" counter(s) in every tin. There are "+numbers[1]+" tin(s). How many are counters altogether?",
       "Liz has "+numbers[0]+" coloured bead(s) in each of her "+numbers[1]+" baskets, how many beads altogether?",
-      "Mary has "+numbers[0]+" earring(s) in each box. There are "+numbers[1]+" boxes. What is the total number of earrings?",
+      "Mary has "+numbers[0]+" earring(s) in each box. There are "+numbers[1]+" box/boxes. What is the total number of earrings?",
       "Albert has "+numbers[0]+" coloured sticker(s) on every page of his book. There are "+numbers[1]+" page(s). How many coloured stickers altogether?",
       "Timothy has "+numbers[1]+" mixed fruit in every bowl. There are "+numbers[1]+" bowl(s). How much mixed fruit altogether?",
       "Tino has "+numbers[0]+" bean(s) in every packet. There are "+numbers[1]+" packet(s). How many beans altogether?",
       "Thuli has "+numbers[0]+" jelly bean(s) in each bag. There are "+numbers[1]+" bags(s). What is the total number of jelly beans?",
       "Tandana has "+numbers[0]+" ball(s) in each basket. There are "+numbers[1]+" basket(s). How many balls altogether?",
       "Marie has "+numbers[0]+" magnet(s) in each drawer. There are "+numbers[1]+" drawer(s). How many drawers altogether?",
-      "Fiona has "+numbers[0]+" coloured rock(s) in each box. There are "+numbers[1]+" boxes. How many coloured rocks altogether?"
+      "Fiona has "+numbers[0]+" coloured rock(s) in each box. There are "+numbers[1]+" box/boxes. How many coloured rocks altogether?"
     ];
   }
 
   var divideQuestions = function(numbers){
     return [
       "Ben has "+numbers[0]+" counter(s) and "+numbers[1]+" friend(s), if Ben shares the counter(s) equally how many can each of them have? There may be some left over.",
-      "David has "+numbers[0]+" coloured bead(s) and "+numbers[1]+" boxes, if David shares the bead(s) equally how many in each box? There may be some left over.",
+      "David has "+numbers[0]+" coloured bead(s) and "+numbers[1]+" box/boxes, if David shares the bead(s) equally how many in each box? There may be some left over.",
       "Mary has "+numbers[0]+" earring(s) and wants to share them equally with her "+numbers[1]+" sister(s), how many does each sister get? There may be some left over.",
       "Pearl has "+numbers[0]+" coloured sticker(s) and wants to put the same number of sticker(s) on "+numbers[1]+" book(s), how many stickers on each book? There may be some left over.",
       "David has "+numbers[0]+" mixed fruit and "+numbers[1]+" friend(s), if David shares the mixed fruit equally how many can each of them have? There may be some left over that David can eat.",
-      "Rita havs "+numbers[0]+" bean(s) and "+numbers[1]+" boxes. If Rita shares the bean(s) equally how many in each box? There may be some left over.",
+      "Rita havs "+numbers[0]+" bean(s) and "+numbers[1]+" box/boxes. If Rita shares the bean(s) equally how many in each box? There may be some left over.",
       "Thuli has "+numbers[0]+" jelly bean(s) and "+numbers[1]+" brother(s), if Thuli shares the bean(s) equally how many can each of them have? There may be some left over.",
       "Tandana has "+numbers[0]+" ball(s) and "+numbers[1]+" basket(s). If Tandana puts the same number in each basket, how many balls in each basket? There may be some left over.",
       "Marie has "+numbers[0]+" magnet(s) and "+numbers[1]+" fridge(s). If Marie puts the same number of magnet(s) on each fridge, how many magnets are on each fridge? There may be some left over.",
